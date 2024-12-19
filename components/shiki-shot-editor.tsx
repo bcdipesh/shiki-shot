77f2353f244911108418aff54c6e3c5ea12d3872
Dipesh B C <bcdipeshwork@gmail.com>
2024-12-18 21:09:33 -0500

    feat: add feature to copy shareable url
    Add a feature to copy shareable url of the current state of the editor

--- components/shiki-shot-editor.tsx
+++ components/shiki-shot-editor.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useSearchParams, usePathname } from "next/navigation";
 import type { JSX } from "react";
 import { useState, useRef, useLayoutEffect } from "react";
 import {
@@ -14,11 +15,31 @@ import { toast } from "sonner";
 import { highlight } from "@/lib/shiki-highlighter";
 import { Combobox } from "@/components/ui/combobox";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+} from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenuGroup,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
 
 export function ShikiShotEditor() {
-  const [theme, setTheme] = useState<BundledTheme>("vitesse-dark");
-  const [lang, setLang] = useState<BundledLanguage>("typescript");
-  const [input, setInput] = useState("// Type your code here");
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const selectedTheme =
+    (searchParams.get("theme") as BundledTheme) || "vitesse-dark";
+  const selectedLang =
+    (searchParams.get("lang") as BundledLanguage) || "typescript";
+  const code = searchParams.get("code") || "// Type your code here";
+
+  const [theme, setTheme] = useState<BundledTheme>(selectedTheme);
+  const [lang, setLang] = useState<BundledLanguage>(selectedLang);
+  const [input, setInput] = useState(code);
   const [highlightedCode, setHighlightedCode] = useState<JSX.Element>();
   const codeEditorContainerRef = useRef<HTMLDivElement>(null);
   const codeRef = useRef<HTMLDivElement>(null);
@@ -37,7 +58,7 @@ export function ShikiShotEditor() {
     highlight(input, lang, theme).then((output) => setHighlightedCode(output));
   }, [input, lang, theme]);
 
-  const captureCodeSnapshot = async () => {
+  const copyCodeImageToClipboard = async () => {
     if (!codeEditorContainerRef.current) return;
 
     const codeCanvas = await html2canvas(codeEditorContainerRef.current, {
@@ -63,12 +84,37 @@ export function ShikiShotEditor() {
         );
         const data = [new ClipboardItem({ "image/png": blob })];
         await navigator.clipboard.write(data);
-        toast("Code snapshot copied to clipboard!");
+        toast(
+          "📸 Snippet snapped! Your image is polished and ready in the clipboard. 🚀",
+        );
       } catch (error) {
-        toast(`Failed to copy code snapshot to clipboard: ${error}`);
-        console.error("Failed to copy code snapshot to clipboard:", error);
+        toast(
+          `❌ Yikes! Couldn't copy the image. Debugging time: ${(error as Error).message}`,
+        );
+        console.error("Failed to copy image to the clipboard:", error);
       }
     };
+  };
+
+  const getShareableUrl = async () => {
+    const params = new URLSearchParams(searchParams);
+    params.set("code", input);
+    params.set("theme", theme);
+    params.set("lang", lang);
+
+    const shareableUrl = `${window.location.origin}${pathname}?${params.toString()}`;
+
+    try {
+      await navigator.clipboard.writeText(shareableUrl);
+      toast(
+        "🔗 Link generated! Share the beauty of your code with the world. 🌐",
+      );
+    } catch (error) {
+      toast(
+        `💥 Uh-oh! The link didn't make it. Error: ${(error as Error).message}`,
+      );
+      console.error("Failed to copy URL to clipboard:", error);
+    }
   };
 
   const handleScroll = () => {
@@ -78,6 +124,31 @@ export function ShikiShotEditor() {
 
     codeRef.current.scrollLeft = editorRef.current.scrollLeft;
     codeRef.current.scrollTop = editorRef.current.scrollTop;
+  };
+
+  const handleThemeChange = (value: string) => {
+    const params = new URLSearchParams(searchParams);
+
+    setTheme(value as BundledTheme);
+
+    if (value) {
+      params.set("theme", value);
+    } else {
+      params.delete("theme");
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = event.target;
+    const params = new URLSearchParams(searchParams);
+
+    setInput(value);
+
+    if (value) {
+      params.set("code", value);
+    } else {
+      params.delete("code");
+    }
   };
 
   return (
@@ -90,8 +161,12 @@ export function ShikiShotEditor() {
             value: theme.id,
             label: theme.displayName,
           }))}
-          selectedValue={{ value: theme, label: "Vitesse Dark" }}
-          onValueChange={(value) => setTheme(value as BundledTheme)}
+          selectedValue={{
+            value: theme,
+            label: bundledThemesInfo.find(({ id }) => id === selectedTheme)
+              ?.displayName as string,
+          }}
+          onValueChange={handleThemeChange}
           placeholder="Select a theme"
         />
 
@@ -106,8 +181,24 @@ export function ShikiShotEditor() {
           placeholder="Select a language"
         />
 
-        {/* Capture Snapshot */}
-        <Button onClick={captureCodeSnapshot}>Capture Code Snapshot</Button>
+        {/* Clipboard Actions */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button>Copy to Clipboard</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuLabel>Available Options</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem onClick={copyCodeImageToClipboard}>
+                Image
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={getShareableUrl}>
+                Shareable URL
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
 
       {/* Editor & Preview */}
@@ -127,7 +218,8 @@ export function ShikiShotEditor() {
         {/* Transparent Input Layer */}
         <textarea
           ref={editorRef}
-          onChange={(e) => setInput(e.target.value)}
+          defaultValue={input}
+          onChange={handleInputChange}
           onScroll={handleScroll}
           className="absolute inset-0 z-10 h-full min-h-[25rem] w-full resize-none overflow-hidden rounded-xl bg-transparent p-4 font-mono text-sm leading-7 text-transparent caret-gray-500 outline-none"
           spellCheck="false"
