79ab3d43a66f293249bdddc9b8ba131498233ce8
Dipesh B C <bcdipeshwork@gmail.com>
2024-12-29 21:41:24 -0500

    chore(deps): upgrade next.js, react and react dom to latest stable

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,11 @@
-// @ts-check
-import withSerwistInit from "@serwist/next";
-
-// You may want to use a more robust revision to cache
-// files more efficiently.
-// A viable option is `git rev-parse HEAD`.
-const revision = crypto.randomUUID();
-
-const withSerwist = withSerwistInit({
-  cacheOnNavigation: true,
-  swSrc: "app/sw.ts",
-  swDest: "public/sw.js",
-  additionalPrecacheEntries: [{ url: "/~offline", revision }],
-});
-
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
-    ignoreBuildErrors: true, // Ignore build errors until production
+    ignoreBuildErrors: true,
   },
 };
 
 const withVercelToolbar = require("@vercel/toolbar/plugins/next")();
 
-export default withSerwist(withVercelToolbar(nextConfig));
+export default withVercelToolbar(nextConfig);
